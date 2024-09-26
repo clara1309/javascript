abae252bec30f101ae6b8eb4cc0d620ba331febf
clara1309 <rodriguesnunesa7@gmail.com>
2024-09-26 15:11:39 -0400

    teste

--- calcule imc/index.js
+++ calcule imc/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 
+const calculadoraIMC = require('./calculadoraIMC');
+
 app.get('/', (req, res) => {
     let peso = req.query.peso;
     let altura = req.query.altura;
 
-    let imc = peso / (altura * altura);
+    if (calculadoraIMC.validaParametro(req.query.peso) && calculadoraIMC.validaParametro(req.query.altura)){
+    let imc = calculadoraIMC.efetuarCauculoIMC(peso, altura);
+    let status = calculadoraIMC.retornarSastusIMC(imc);
+    
+    res.json({imc: imc, status: status});
+    }
+else {
+    res.status(400).json ({'Erro': 'Peso ou altura invalidos'});
+    }
 
-    res.json({imc: imc});
-})
+});
 
 app.listen(8080, () => {
     let data = new Date();
